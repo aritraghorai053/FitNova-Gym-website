45619d20bc757bcb5d91ea9adfef9bfdcbd41f0c
Aritra Ghorai <aritraghorai879@gmail.com>
2026-08-18 17:33:10 +0530

    Add menu toggle functionality to navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListner("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("FitNova website loaded!");
 });
